0ee3c7e55a0cfb79bcf8184b4e4578c7bd4fcb18
Christian Peraza <christianperaza2000@gmail.com>
2024-11-21 10:47:57 -0300

    Name inventory tags

--- utilities/index.js
+++ utilities/index.js
@@ -32,21 +32,21 @@ Util.buildClassificationGrid = async function(data){
     if(data.length > 0){
         grid = '<ul id="inv-display">'
         data.forEach(vehicle => { 
-            grid += '<li>'
-            grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+            grid += '<li class="invItems">'
+            grid +=  '<a class="invImgContainer" href="../../inv/detail/'+ vehicle.inv_id 
             + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-            + 'details"><img src="' + vehicle.inv_thumbnail 
+            + 'details"><img class="invImg" src="' + vehicle.inv_thumbnail 
             +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
             +' on CSE Motors" /></a>'
             grid += '<div class="namePrice">'
-            grid += '<hr />'
+            grid += '<hr class="invLine" />'
             grid += '<h2>'
-            grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+            grid += '<a class="invNameLink" href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
             + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
             + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
             grid += '</h2>'
-            grid += '<span>$' 
-            + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+            grid += '<p class="invPrice">$' 
+            + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</p>'
             grid += '</div>'
             grid += '</li>'
         })

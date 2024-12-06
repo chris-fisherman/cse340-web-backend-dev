5b5d8d877b94db3aa599813814d50c8a3ea727bf
Christian Peraza <christianperaza2000@gmail.com>
2024-12-06 09:26:23 -0300

    Add insert inventory

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -53,4 +53,56 @@ async function insertClassification(classification_name) {
     }
 }
 
-module.exports = {getClassifications, getInventoryByClassificationId, getInventoryByInvId, insertClassification}
+/* ***************************
+ *  Insert New Inventory
+ * ************************** */
+async function insertInventory(
+    inv_make,
+    inv_model,
+    inv_year,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_miles,
+    inv_color,
+    classification_id) {
+    try {
+        const sql = `INSERT INTO public.inventory (
+            inv_make,
+            inv_model,
+            inv_year,
+            inv_description,
+            inv_image,
+            inv_thumbnail,
+            inv_price,
+            inv_miles,
+            inv_color,
+            classification_id
+        ) VALUES (
+            $1,
+            $2,
+            $3,
+            $4,
+            $5,
+            $6,
+            $7,
+            $8,
+            $9,
+            $10) RETURNING *`
+        return await pool.query(sql, [inv_make,
+            inv_model,
+            inv_year,
+            inv_description,
+            inv_image,
+            inv_thumbnail,
+            inv_price,
+            inv_miles,
+            inv_color,
+            classification_id])
+    } catch (error) {
+        return error.message
+    }
+}
+
+module.exports = {getClassifications, getInventoryByClassificationId, getInventoryByInvId, insertClassification, insertInventory}

98d909a80be27aadda8e8d34920d72a23164847f
Christian Peraza <christianperaza2000@gmail.com>
2024-12-06 09:26:05 -0300

    Add inventory post

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,6 +4,7 @@ const router = new express.Router()
 const invController = require("../controllers/invController")
 const utilities = require("../utilities/")
 const classificationValidate = require('../utilities/newClassification-validation')
+const inventoryValidate = require('../utilities/addInventory-validation')
 
 // Route to build management view
 router.get("/", utilities.handleErrors(invController.buildManageInv));
@@ -26,5 +27,12 @@ router.post(
 
 // Add inventory route
 router.get("/add-inventory", utilities.handleErrors(invController.buildAddInventory));
+// post
+router.post(
+    "/add-inventory",
+    inventoryValidate.inventoryRules(),
+    inventoryValidate.checkInvData,
+    utilities.handleErrors(invController.registerNewInventory)
+);
 
 module.exports = router;

e5bc902fc3d7fd08fc2cf796d22cb60ba0513052
Christian Peraza <christianperaza2000@gmail.com>
2024-12-11 13:44:06 -0300

    Add class name to select classification

--- utilities/index.js
+++ utilities/index.js
@@ -98,7 +98,7 @@ Util.buildClassificationList = async function (classification_id = null) {
     let data = await invModel.getClassifications()
     let classificationList = '<label for="classification_id">Classification:</label><br>'
     classificationList +=
-      '<select name="classification_id" id="classification_id" required>'
+      '<select name="classification_id" id="classification_id" class="classificationList" required>'
     classificationList += "<option value=''>Choose a Classification</option>"
     data.rows.forEach((row) => {
       classificationList += '<option value="' + row.classification_id + '"'

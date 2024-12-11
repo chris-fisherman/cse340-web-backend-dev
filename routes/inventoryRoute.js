e8742eeeb3343582ec477e8d4b30f9877369069a
Christian Peraza <christianperaza2000@gmail.com>
2024-12-11 13:44:33 -0300

    Add getInventory route

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -35,4 +35,10 @@ router.post(
     utilities.handleErrors(invController.registerNewInventory)
 );
 
+// Get inventory route
+router.get(
+    "/getInventory/:classification_id", 
+    utilities.handleErrors(invController.getInventoryJSON)
+)
+
 module.exports = router;
